0e5a9a3545767f7cfda2fa9fd320e02cc89e07e0
marant7 <mf2018000597@virtual.upt.pe>
2025-04-26 23:43:24 -0500

    resolviendo

---
+++ ATM/ATM.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ATM.Domain.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
